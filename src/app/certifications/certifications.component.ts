21e3f2e61f66494958ea952fd380c943fc07ddbe
RamMurari99 <rammurari3528@gmail.com>
2025-05-28 18:15:42 +0530

    profile changes

--- src/app/certifications/certifications.component.ts
+++ src/app/certifications/certifications.component.ts
@@ -7,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './certifications.component.scss'
 })
 export class CertificationsComponent {
+  path:string="assets/js_certification.png"
+  path2:string="assets/ramMurari Front End Development certification.png"
 
 }
